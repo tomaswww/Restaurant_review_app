9111f7dc5c40fbf4228a76e86db113ab1dc27fac
tomaswww <twingord@gmail.com>
2018-11-19 22:58:45 -0300

    Eliminate added js command to set tabindex for map which was not working

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ initMap = () => {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
+    id: 'mapbox.streets',
   }).addTo(newMap);
 
   updateRestaurants();
