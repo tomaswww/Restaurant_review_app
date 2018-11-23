161efc962f48fbe77f9b5c670049218c33079168
tomaswww <twingord@gmail.com>
2018-11-23 19:38:09 -0300

    Fixed semantic mistake suggested in restaurant name for main js file

--- js/main.js
+++ js/main.js
@@ -166,7 +166,7 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   name.tabIndex=0;
   li.append(name);
